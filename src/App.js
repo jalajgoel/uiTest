397eac55bab5854759810c79e56dd3a041f30a95
jalajgoel <jalajgoel88@gmail.com>
2022-05-25 20:24:42 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductDetail from './component/ProductDetail';
-import DataList from './component/dataList';
 
 function App() {
   return (
     <div className="App">
-      {/* <ProductDetail/> */}
-      <DataList/>
+      <ProductDetail/>
     </div>
   );
 }
